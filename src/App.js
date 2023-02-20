3890c787bf9e0530ebedc64b1e80dbb9b28e1836
Hariram9932 <124166880+Hariram9932@users.noreply.github.com>
2023-02-20 13:29:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div ><center>
+      <h1> HR motorcycles</h1>
+      <p>ONe Stop Shop for your moter cycle needs</p>
+      <p><img src="https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/classic-350/landing/classic-350-motorcycle.jpg"></img></p>
+    </center>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/img.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './img.css';
+
+function Img() {
+  return (
+    <div >
+      <h1></h1>
+      <center><p> Buy this Bike for 1000 Rs <br/> Click Here</p>
+      <button type="button">Submit</button>
+      {/* <p><img src="https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/classic-350/landing/classic-350-motorcycle.jpg"></img></p> */}
+      <p id='des'> Welcome to HR group of motors. We here are destined to produce top notch motors for our coustomers.<br/> 
+      After our tremondous success in countries like USA and CANADA we are now launching on India as well.<br/>
+      We will be launching a super bike that will both run on petrol and battery according to the needs of the user </p>
+      
+      </center>
+    </div>
+    
+  );
+}
+
+export default Img;
